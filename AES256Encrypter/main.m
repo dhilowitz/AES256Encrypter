fa58374c542e476bf35ca6b3887333739d50269c
David Hilowitz <dhilowitz@gmail.com>
2014-03-29 20:18:08 -0400

    Everything seems to work. I have done no testing on this at all.

--- AES256Encrypter/main.m
+++ AES256Encrypter/main.m
@@ -9,19 +9,32 @@
 #import <Foundation/Foundation.h>
 #import "NSDataAES256.h"
 
+void printUsage () {
+    NSLog(@"Correct usage:\n\nAES256Encrypter.app [-d|-e] <key> <input file> <output file>\nOptions:\n\t-d = decrypt\n\t-e = encrypt\n");
+}
+
+
 int main(int argc, const char * argv[])
 {
 
     @autoreleasepool {
         
-        if(argc != 4) {
+        if(argc != 5) {
             // insert code here...
-            NSLog(@"\nMissing file name.\nUsage: AES256Encryper.app <key> <in-file> <out-file>\n");
+            NSLog(@"\nMissing file name.\n");
+            printUsage();
             return 1;
         } else {
-            NSString *key = [NSString stringWithUTF8String:argv[1]];
-            NSString *inputPath = [NSString stringWithUTF8String:argv[2]];
-            NSString *outputPath = [NSString stringWithUTF8String:argv[3]];
+            NSString *command = [NSString stringWithUTF8String:argv[1]];
+            if(![command isEqualToString:@"-e"] && ![command isEqualToString:@"-d"]) {
+                NSLog(@"\nInvalid command.");
+                printUsage();
+                return 1;
+            }
+                
+            NSString *key = [NSString stringWithUTF8String:argv[2]];
+            NSString *inputPath = [NSString stringWithUTF8String:argv[3]];
+            NSString *outputPath = [NSString stringWithUTF8String:argv[4]];
 
             NSData* loadedData = [[NSData alloc] initWithContentsOfFile:inputPath];
             if(loadedData == nil) {
@@ -30,11 +43,17 @@ int main(int argc, const char * argv[])
             }
             
             NSData* keyData = [key dataUsingEncoding:NSUTF8StringEncoding];
-            NSData *encData = [loadedData encryptedWithKey:keyData];
-            [encData writeToFile:outputPath atomically:YES];
+            NSData *newData;
+            if ([command isEqualToString:@"-e"]) {
+                newData = [loadedData encryptedWithKey:keyData];
+            } else {
+                newData = [loadedData decryptedWithKey:keyData];
+            }
+            [newData writeToFile:outputPath atomically:YES];
         }
         
     }
     return 0;
 }
+
 
